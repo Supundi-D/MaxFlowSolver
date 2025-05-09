ddf7fbfbc0df207b6df97cec589535bcb9ce0c8a
Supundi Dinaya <supundidinaya@gmail.com>
2025-05-09 09:20:11 +0530

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,42 @@
+/* Student Name : Supundi Goonarathne
+IIT ID : 20230621
+UOW ID : w2083105
+*/
+
+public class Edge {
+    private final int from;
+    private final int to;
+    private final int capacity;
+    private int flow;
+
+    public Edge(int from, int to, int capacity) {
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getFlow() {
+        return flow;
+    }
+
+    public void setFlow(int flow) {
+        this.flow = flow;
+    }
+
+    public int getResidualCapacity() {
+        return capacity - flow;
+    }
+}
